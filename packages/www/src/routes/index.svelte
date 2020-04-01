e7b3fbd5338b6a8774b67b5d260de0b9eee20259
w27730cs <cosmin.serdean@student.manchester.ac.uk>
2020-04-01 03:52:52 +0300

    Add windows link

--- packages/www/src/routes/index.svelte
+++ packages/www/src/routes/index.svelte
@@ -1,77 +1,33 @@
 <script>
-  import { onMount } from 'svelte'
-  import { fly } from 'svelte/transition'
-  import VanillaTilt from 'vanilla-tilt'
-  import { clickOutside } from '../utils/clickOutside.js'
+  import { onMount } from "svelte";
+  import { fly } from "svelte/transition";
+  import VanillaTilt from "vanilla-tilt";
+  import { clickOutside } from "../utils/clickOutside.js";
 
   let dropdown = false;
 
   const onDropDownClick = () => {
-    dropdown = !dropdown
-  }
+    dropdown = !dropdown;
+  };
 
   const onDropDownClickOutside = () => {
-    dropdown = false
-  }
+    dropdown = false;
+  };
 
   onMount(() => {
-    const screenshot = document.querySelectorAll(".tilt")
+    const screenshot = document.querySelectorAll(".tilt");
 
     VanillaTilt.init(screenshot, {
       max: 15,
       speed: 400,
       glare: true,
-      'max-glare': .5,
-      scale: 1.05,
+      "max-glare": 0.5,
+      scale: 1.05
     });
 
-    return () => screenshot.vanillaTilt.destroy() 
-  })
-
+    return () => screenshot.vanillaTilt.destroy();
+  });
 </script>
-<svelte:head>
-	<title>bench - your new workspace</title>
-  <meta name="description" content="Bench is a single app that meets all your online communication needs.">
-</svelte:head>
-
-<main>
-  <div class="hero">
-    <div class="title-wrapper">
-      <h1>
-        Meet <img src="logo.svg" alt="logo" class="logo"> 
-      </h1>
-      <h2>your new workspace</h2>
-      <div class="tilt img-wrapper-mobile">
-        <img src="/screenshot-sh.png" alt="screenshot" class="screenshot-mobile">
-      </div>
-      <p>Bench is a single app that meets all your online communication needs.</p>
-      <button class="download" class:dropdown-open={dropdown} on:click={onDropDownClick} use:clickOutside on:click_outside={onDropDownClickOutside}>
-        <img src="/chevron-down.svg" alt="chevron" class="chevron">Download
-        {#if dropdown}
-          <div class="dropdown" transition:fly="{{y: -8, duration: 300}}" >
-            <a href="http://hazel.bench.cserdean.com/download/dmg" class="dropdown-item">
-              <img src="/apple-logo.svg" alt="apple-logo" class="download-logo">
-              for Mac
-            </a>
-            <a href="" class="dropdown-item">
-              <img src="/windows-logo.svg" alt="windows-logo" class="download-logo">
-              for Windows
-            </a>
-            <a href="" class="dropdown-item">
-              <img src="/ubuntu-logo.svg" alt="ubuntu-logo" class="download-logo">
-              for Ubuntu
-            </a>
-          </div>
-        {/if}
-      </button>
-      <a href="https://cserdean.com" class="by">Built by cserdean.com</a>
-    </div>
-    <div class="img-wrapper tilt">
-      <img src="/screenshot-sh.png" alt="Screenshot" class="screenshot">
-    </div>
-  </div>
-  <svg viewBox="0 0 100 100" class="sc-kgoBCf iQWuJD"><polygon points="55,0 100,0 100,100 55,100" fill="url(#prefix__paint0_linear)"></polygon><defs><linearGradient id="prefix__paint0_linear" x1="100" y1="100" x2="33" y2="100" gradientUnits="userSpaceOnUse"><stop stop-color="#222"></stop><stop offset="1" stop-color="#111"></stop></linearGradient></defs></svg>
-</main>
 
 <style>
   main {
@@ -88,7 +44,7 @@
 
   h1 {
     margin: 0;
-    margin-bottom: .5rem;
+    margin-bottom: 0.5rem;
   }
 
   h2 {
@@ -102,13 +58,13 @@
   }
 
   .tilt {
-    border-radius: .25rem;
+    border-radius: 0.25rem;
     overflow: hidden;
   }
 
   .img-wrapper {
     flex: 1.5;
-    margin: .5rem;
+    margin: 0.5rem;
   }
 
   .screenshot {
@@ -137,8 +93,8 @@
 
   .download {
     color: var(--primary);
-    padding: .8rem 0;
-    border-radius: .25rem;
+    padding: 0.8rem 0;
+    border-radius: 0.25rem;
     background: var(--background);
     cursor: pointer;
     display: flex;
@@ -154,16 +110,16 @@
   .dropdown {
     position: absolute;
     background: var(--foreground);
-    top: calc(100% + .6rem);
+    top: calc(100% + 0.6rem);
     left: 0;
     right: 0;
-    border-radius: .25rem;
+    border-radius: 0.25rem;
     /* border-top-left-radius: 0; */
     /* border-top-right-radius: 0; */
   }
 
   .dropdown:before {
-    content: '';
+    content: "";
     display: inline-block;
     position: absolute;
     height: 0.5rem;
@@ -180,18 +136,18 @@
   .dropdown-item {
     display: flex;
     color: var(--background) !important;
-    padding: .75rem 1rem;
+    padding: 0.75rem 1rem;
     text-decoration: none;
   }
 
   .dropdown-item:first-child {
-    border-top-left-radius: .25rem;
-    border-top-right-radius: .25rem;
+    border-top-left-radius: 0.25rem;
+    border-top-right-radius: 0.25rem;
   }
 
   .dropdown-item:last-child {
-    border-bottom-left-radius: .25rem;
-    border-bottom-right-radius: .25rem;
+    border-bottom-left-radius: 0.25rem;
+    border-bottom-right-radius: 0.25rem;
   }
 
   .dropdown-item:not(:last-child) {
@@ -204,11 +160,11 @@
 
   .download-logo {
     height: 1rem;
-    margin-right: .5rem;
+    margin-right: 0.5rem;
   }
 
   .chevron {
-    margin-right: .5rem;
+    margin-right: 0.5rem;
     height: 1rem;
     vertical-align: text-bottom;
     display: none;
@@ -225,18 +181,17 @@
     text-decoration: underline;
   }
 
-
   .screenshot-mobile {
     display: none;
   }
-
 
   @media screen and (max-width: 992px) {
     .hero {
       align-items: flex-end;
     }
-    .img-wrapper, svg {
-      display: none; 
+    .img-wrapper,
+    svg {
+      display: none;
     }
 
     .title-wrapper {
@@ -259,14 +214,15 @@
       height: 2.5rem;
     }
 
-    p, h1, h2 {
+    p,
+    h1,
+    h2 {
       text-align: center;
     }
 
     .screenshot-mobile {
       width: 100%;
       display: block;
-
     }
 
     .img-wrapper-mobile {
@@ -280,9 +236,96 @@
     .hero {
       align-items: center;
     }
-    
+
     .title-wrapper {
       padding-bottom: 0;
     }
   }
 </style>
+
+<svelte:head>
+  <title>bench - your new workspace</title>
+  <meta
+    name="description"
+    content="Bench is a single app that meets all your online communication
+    needs." />
+</svelte:head>
+
+<main>
+  <div class="hero">
+    <div class="title-wrapper">
+      <h1>
+        Meet
+        <img src="logo.svg" alt="logo" class="logo" />
+      </h1>
+      <h2>your new workspace</h2>
+      <div class="tilt img-wrapper-mobile">
+        <img
+          src="/screenshot-sh.png"
+          alt="screenshot"
+          class="screenshot-mobile" />
+      </div>
+      <p>
+        Bench is a single app that meets all your online communication needs.
+      </p>
+      <button
+        class="download"
+        class:dropdown-open={dropdown}
+        on:click={onDropDownClick}
+        use:clickOutside
+        on:click_outside={onDropDownClickOutside}>
+        <img src="/chevron-down.svg" alt="chevron" class="chevron" />
+        Download
+        {#if dropdown}
+          <div class="dropdown" transition:fly={{ y: -8, duration: 300 }}>
+            <a
+              href="http://hazel.bench.cserdean.com/download/dmg"
+              class="dropdown-item">
+              <img
+                src="/apple-logo.svg"
+                alt="apple-logo"
+                class="download-logo" />
+              for Mac
+            </a>
+            <a
+              href="http://hazel.bench.cserdean.com/download/exe"
+              class="dropdown-item">
+              <img
+                src="/windows-logo.svg"
+                alt="windows-logo"
+                class="download-logo" />
+              for Windows
+            </a>
+            <!--
+            <a href="" class="dropdown-item">
+              <img src="/ubuntu-logo.svg" alt="ubuntu-logo" class="download-logo">
+              for Ubuntu
+            </a>
+            !-->
+          </div>
+        {/if}
+      </button>
+      <a href="https://cserdean.com" class="by">Built by cserdean.com</a>
+    </div>
+    <div class="img-wrapper tilt">
+      <img src="/screenshot-sh.png" alt="Screenshot" class="screenshot" />
+    </div>
+  </div>
+  <svg viewBox="0 0 100 100" class="sc-kgoBCf iQWuJD">
+    <polygon
+      points="55,0 100,0 100,100 55,100"
+      fill="url(#prefix__paint0_linear)" />
+    <defs>
+      <linearGradient
+        id="prefix__paint0_linear"
+        x1="100"
+        y1="100"
+        x2="33"
+        y2="100"
+        gradientUnits="userSpaceOnUse">
+        <stop stop-color="#222" />
+        <stop offset="1" stop-color="#111" />
+      </linearGradient>
+    </defs>
+  </svg>
+</main>
